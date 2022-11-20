4d1c425494c323ba959525fdb6fccb6048cdc3e2
zsombialmasi <zsombi.almasi@gmail.com>
2022-11-20 21:12:09 +0100

    ipynb for stable baselines3 training

--- deprecatedtown/training/utils/wrappers.py
+++ deprecatedtown/training/utils/wrappers.py
@@ -83,6 +83,20 @@ class ImgWrapper(gym.ObservationWrapper):
     def observation(self, observation):
         return observation.transpose(2, 0, 1)
 
+class ImgWrapper2(gym.ObservationWrapper):
+    def __init__(self, env=None):
+        super(ImgWrapper, self).__init__(env)
+        obs_shape = self.observation_space.shape
+        self.observation_space = spaces.Box(
+            self.observation_space.low[0, 0, 0],
+            self.observation_space.high[0, 0, 0],
+            [obs_shape[2], obs_shape[1], obs_shape[0]],
+            dtype=self.observation_space.dtype,
+        )
+
+    def observation(self, observation):
+        return observation.transpose(2, 0, 1)
+
 
 class DtRewardWrapper(gym.RewardWrapper):
     def __init__(self, env):
